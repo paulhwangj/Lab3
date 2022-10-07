0b7fa4d92596446bd52509585ac3e7c689a42141
paul <105162443+pl8nt@users.noreply.github.com>
2022-10-07 10:45:00 -0500

    initial commit

---
+++ MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿namespace Lab2Solution;
+
+/**
+Name: XEE LO
+Date: OCTOBER 6, 2022
+Description: CS 341 SOFTWARE ENGINEERING: LAB 3
+Bugs: 
+Reflection: 
+*/
+public static class MauiProgram
+{
+    public static IBusinessLogic ibl = new BusinessLogic();
+
+    public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		return builder.Build();
+	}
+}
+
